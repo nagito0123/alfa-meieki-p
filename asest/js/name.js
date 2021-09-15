cd8d9886712383cc7ad7bf267e2ae37413e3e5f3
yosssyyy <89887568+yosssyyy@users.noreply.github.com>
2021-09-15 11:27:57 +0900

    Create name.js

---
+++ asest/js/name.js
@@ -0,0 +1,11 @@
+const storage = localStorage;
+
+document.getElementById("game_start").addEventListener("click",function(){
+    const name = document.getElementById("input_name").value;
+    if(name == ""){
+        window.alert("ニックネームを入力してください");
+        return;
+    }
+    storage.setItem("name", name);
+    window.location.href = './main.html';
+});
